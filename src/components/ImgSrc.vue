dbb62491f37fa009e4da2f9b25c763008cf3cc5e
White Pai <white_pai@appdet.com>
2022-02-21 22:26:02 +0800

    feat: add imgSrc component

---
+++ src/components/ImgSrc.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: "png"
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: Number,
+    default: 0,
+  },
+  height: {
+    type: Number,
+    default: 0,
+  },
+  classString: {
+    type: String,
+    default: ""
+  }
+});
+</script>
+
+<template>
+  <img
+    :srcset="`${name}.${type} 1x, ${name}@2x.${type} 2x, ${name}@3x.${type} 3x,`"
+    :src="`${name}.${type}`"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    :class="classString"
+  >
+</template>
